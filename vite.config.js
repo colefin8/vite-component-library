3c192a3a4fa85088f6e3eabe423496c51474a403
Cole Finlayson <55099198+colefin8@users.noreply.github.com>
2023-03-27 15:05:24 -0600

    built and compiled

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,53 @@
-import { defineConfig } from 'vite'
+import fs from 'fs'
+import alias from '@rollup/plugin-alias';
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import scss from 'rollup-plugin-scss'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import path from 'path'
+
+const projectRoot = path.resolve(__dirname, '');
 
 export default defineConfig({
-  plugins: [vue()],
+  input: '/src/entry.js',
+  plugins: [
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: `${path.resolve(projectRoot, 'src')}`,
+        },
+      ],
+    }),
+    vue(),
+    resolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+    }),
+    commonjs(),
+    scss(),
+    babel({      
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+      babelHelpers: 'bundled',
+      presets: [
+        '@babel/preset-env',
+        // Config for @babel/preset-env
+        {
+          // Example: Always transpile optional chaining/nullish coalescing
+          include: [
+            /(optional-chaining|nullish-coalescing)/
+          ],
+        },
+      ],
+    }),
+  ],
   build: {
+    target: 'es2016',
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/main.js'),
+      entry: path.resolve(__dirname, 'src/entry.js'),
       name: 'AcoDesignSystem',
       // the proper extensions will be added
       fileName: 'aco-design-system',
